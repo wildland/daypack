24d8c3a873f454ba80036acc7a4e98044f4c398d
Ty Mulholland <ty@wild.land>
2017-07-12 22:50:52 -0700

    Update setup-osx.sh

--- setup-osx.sh
+++ setup-osx.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "Running Ruby script to finish development setup"
-echo `ruby -e "$(curl -fsSL https://raw.githubusercontent.com/wildland/daypack/setup-osx.rb)"`
+echo `ruby -e "$(curl -fsSL https://raw.githubusercontent.com/wildland/daypack/master/setup-osx.rb)"`
